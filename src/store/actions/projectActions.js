80de61db07bf2d3d8184a24c13202da2ebfa49ae
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-09-08 17:57:51 -0400

    add project to firebase databse

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,16 @@
 export const createProject = (project) => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to database
-    dispatch({ type: 'CREATE_PROJECT', project })
+    const firestore = getFirestore();
+    firestore.collection('projects').add({
+      ...project,
+      authorFirstName: 'Claudeth',
+      authorLastName: 'Casas',
+      authorId: 12345,
+      createdAt: new Date()
+    }).then(() => {
+      dispatch({ type: 'CREATE_PROJECT', project })
+    }).catch((err) => dispatch({ type: 'CREATE_PROJECT_ERROR', err }) )
+    
   }
 }
